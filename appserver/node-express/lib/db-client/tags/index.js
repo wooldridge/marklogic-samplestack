10bf8d2f9ee7e0672cb7d6a6b09b972d2b4d3c66
Mike Wooldridge <mike@hellion.org>
2015-03-26 16:26:49 -0700

    node: add facet constraint support for tags

--- appserver/node-express/lib/db-client/tags/index.js
+++ appserver/node-express/lib/db-client/tags/index.js
@@ -1,17 +1,44 @@
+var qb = require('marklogic').queryBuilder;
 var vb = require('marklogic').valuesBuilder;
+var parseSearchQuery = require('./parseSearchQuery');
 
 var funcs = {};
 
 // For typeahead (filtered) and ask tags (not filtered)
 funcs.getTags = function (spec) {
 
+  var parsedQuery = parseSearchQuery(spec);
+
+  // facet constraints
+  var facets = [];
+  if (parsedQuery.user) {
+    facets.push(qb.value('displayName', parsedQuery.user));
+  }
+  if (parsedQuery.resolved) {
+    facets.push(qb.value('accepted', parsedQuery.resolved));
+  }
+  if (parsedQuery.lastActivityGE) {
+    facets.push(qb.range('lastActivityDate', '>=', parsedQuery.lastActivityGE));
+  }
+  if (parsedQuery.lastActivityLT) {
+    facets.push(qb.range('lastActivityDate', '>=', parsedQuery.lastActivityLT));
+  }
+  if (parsedQuery.tags) {
+    for (i = 0; i < parsedQuery.tags.length; ++i) {
+      facets.push(qb.range("tags", parsedQuery.tags[i]));
+    }
+  }
+
   var forTag = spec.forTag;
 
   var valuesQuery = vb.fromIndexes(
     vb.range('tags')
   )
+  .where(qb.and.apply(this, facets))
   .aggregates('count')
   .withOptions({values: ["frequency-order", "descending", "limit=10"]});
+
+  console.log(JSON.stringify(valuesQuery, null, ' '));
 
   // TODO In 8.0-1, CANNOT include parse* information with Node Client values
   // call, thus we can't get tags constrained to current search qtext/facets
@@ -33,7 +60,6 @@ funcs.getTags = function (spec) {
     }
     response['values-response']['distinct-value'] = distVals;
     delete response['values-response']['tuple'];
-    console.log(JSON.stringify(response['values-response'], null, ' '));
     return response;
   });
 
