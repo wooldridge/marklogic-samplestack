cadc45f9200f5db60095809adcf306171e77b377
Mike Wooldridge <mike@hellion.org>
2015-04-01 16:47:39 -0700

    node: related-tags functionality
    Handle related-tag array returned from first resource call.
    Filter out tags from second values call (which includes counts) that aren't in first call.
    Second related-tag call depends on hookStartRequest just like forTag functionality.
    Make payload that is returned consistent with what is returned from Java Tier.

--- appserver/node-express/lib/db-client/tags/hookStartRequest.js
+++ appserver/node-express/lib/db-client/tags/hookStartRequest.js
@@ -3,10 +3,12 @@ var origStartRequest = mlrest.startRequest;
 var util = require('util');
 
 mlrest.startRequest = function (operation) {
+  // Handle forTag call
   if (
     operation.options.path === '/v1/search?format=json&category=content' &&
     operation.requestBody && operation.requestBody.search &&
-    operation.requestBody.search.forTag
+    (operation.requestBody.search.forTag ||
+    operation.requestBody.search.relatedTo)
   ) {
     operation.options.path = '/v1/values/tags?' +
         'pageLength=10000&options=tags&start=1&aggregate=count';
