3b73b555f96cc231818b5b0b2107170ecb7a4429
Mike Wooldridge <mike@hellion.org>
2015-04-05 23:30:37 -0700

    node: for tags hookStartRequest, consolidate forTags and relatedTags handling
    Also, remove logic that handles resources call in hookStartRequest. Handling this with Node Client resources method.

--- appserver/node-express/lib/db-client/tags/hookStartRequest.js
+++ appserver/node-express/lib/db-client/tags/hookStartRequest.js
@@ -7,37 +7,12 @@ mlrest.startRequest = function (operation) {
   if (
     operation.options.path === '/v1/search?format=json&category=content' &&
     operation.requestBody && operation.requestBody.search &&
-    operation.requestBody.search.forTag
+    (operation.requestBody.search.forTag ||
+    operation.requestBody.search.relatedTo)
   ) {
     operation.options.path = '/v1/values/tags?' +
         'pageLength=10000&options=tags&start=1&aggregate=count';
     operation.options.headers.accept = 'application/json';
-  }
-  // Handle FIRST relatedTag call
-  else if (
-    operation.options.path === '/v1/search?format=json&category=content' &&
-    operation.requestBody && operation.requestBody.search &&
-    operation.requestBody.search.relatedTo &&
-    operation.requestBody.search.qtext.length === 1
-  ) {
-    operation.options.path = '/v1/resources/relatedTags?rs:tag=' +
-      operation.requestBody.search.relatedTo;
-    operation.options.method = 'GET';
-    operation.options.headers.accept = 'application/json';
-    console.log('FIRST relatedTo!!!!');
-
-  }
-  // Handle SECOND relatedTag call
-  else if (
-    operation.options.path === '/v1/search?format=json&category=content' &&
-    operation.requestBody && operation.requestBody.search &&
-    operation.requestBody.search.relatedTo &&
-    operation.requestBody.search.qtext.length === 2
-  ) {
-    operation.options.path = '/v1/values/tags?' +
-        'pageLength=100&options=tags&start=1&aggregate=count';
-    operation.options.headers.accept = 'application/json';
-    console.log('SECOND relatedTo!!!!');
   }
   return origStartRequest(operation);
 };
